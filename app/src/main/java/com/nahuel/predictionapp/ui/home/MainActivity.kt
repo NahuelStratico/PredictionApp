68dbec7222a9fa329943718d043cf14ad0a4cf4d
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2023-10-01 17:10:17 -0300

    initial commit

---
+++ app/src/main/java/com/nahuel/predictionapp/ui/home/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.nahuel.predictionapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nahuel.predictionapp.R
+import com.nahuel.predictionapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.FragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.BottomNavView.setupWithNavController(navController)
+    }
+}
